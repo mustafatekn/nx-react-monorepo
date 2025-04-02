9a9c40abeca71f451f578711f72ec23fbd2c77ca
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2025-04-02 13:32:24 +0300

    add a library as "ui-library"

---
+++ libs/ui-library/src/lib/ui-library.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledUiLibrary = styled.div`
+  color: pink;
+`;
+export function UiLibrary() {
+  return (
+    <StyledUiLibrary>
+      <h1>Welcome to UiLibrary!</h1>
+    </StyledUiLibrary>
+  );
+}
+
+export default UiLibrary;
